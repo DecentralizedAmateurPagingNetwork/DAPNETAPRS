e6d64f3131c6e74262910b3ad32ea0707617cd6d
Ronald Bouwens <rbouwens@localhost.localdomain>
2017-04-26 22:28:16 +0200

    config.py

--- config.py
+++ config.py
@@ -2,23 +2,23 @@
 #store this file in a safe directory, out of range of any kind of server
 
 user = dict(
-	username = "pe2kmv", #your DAPNET username
-	password = "ronald123", #your DAPNET password
-	datenotation = 'nl' #date notation (for weather messages)
+	username = '', #your DAPNET username
+	password = '', #your DAPNET password
+	datenotation = 'nl' #date notation (for weather messages) (en/de/nl)
 )
 
 dapnet = dict(
-	callsign = 'pa-weather', #target callsign for weather messages
-	transmittergrp = 'pa-all', #transmittergroup for weather messages
+	callsign = '', #target callsign for weather messages
+	transmittergrp = '', #transmittergroup for weather messages
 	baseurl = 'http://www.hampager.de:8080', #base url of DAPNET API
 	coreurl = '/calls', #path to send calls
 	trxurl = '/transmitters' #path to query transmitters
 )
 
 aprsis = dict(
-	username = "pe2kmv", #APRS-IS callsign
-	password = "23738", #APRS-IS pass code
-	sourcecall = "PE2KMV", #source call when placing objects
+	username = '', #APRS-IS callsign
+	password = '', #APRS-IS pass code
+	sourcecall = '', #source call when placing objects
 	apikey = "8881.Nba3ye37KpRWrA" #APRS-IS API key
 )
 
